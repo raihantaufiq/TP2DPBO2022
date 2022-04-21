00eacdb466675cc7d7844563560621481a508e02
raihantaufiq <99570231+raihantaufiq@users.noreply.github.com>
2022-04-21 23:27:26 +0700

    Update divisi.php

--- class/divisi.php
+++ class/divisi.php
@@ -3,7 +3,7 @@
 class divisi extends DB{
 	
     function get_AllDivisi(){
-		// Query mysql select data ke tb_to_do
+		
 		$query = "SELECT * FROM divisi";
 
 		// Mengeksekusi query
@@ -11,7 +11,7 @@ class divisi extends DB{
 	}
 
     function get_divisi_byId($id){
-		// Query mysql select data ke tb_to_do
+		
 		$query = "SELECT * FROM divisi WHERE id_divisi=$id";
 
 		// Mengeksekusi query
